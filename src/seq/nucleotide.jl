974015a3f5eaa5f7792bb4a879ff017cd0fc6247
Daniel Jones <dcjones@cs.washington.edu>
2014-08-06 11:32:49 -0700

    Faster nucleotide_count with large numbers of Ns.

--- src/seq/nucleotide.jl
+++ src/seq/nucleotide.jl
@@ -758,40 +758,59 @@ end
 
 # Count number of occourances of each nucleotide and N
 function nucleotide_count{T}(seq::NucleotideSequence{T})
-    d, r = divrem(2 * (seq.part.start - 1), 64)
-    i = 1
+    dn, rn = divrem(seq.part.start - 1, 64)
 
+    d = 2*dn
+    r = 2*dn
+
+    i = 1
     counts = NucleotideCounts{T}()
 
     # count leading unaligned bases
     for i in 1:r
-        counts[getnuc(T, seq.data, seq.part.start + i - 1)] += 1
+        counts[seq[i]] += 1
         i += 1
     end
     if r > 0
         d += 1
     end
 
-    # count aligned bases
-    while i + 31 <= length(seq)
-        counts.a += count_a(seq.data[d + 1])
-        counts.c += count_c(seq.data[d + 1])
-        counts.g += count_g(seq.data[d + 1])
-        counts.t += count_t(seq.data[d + 1])
-        d += 1
-        i += 32
+    # maybe just skip over blocks of Ns as I go?
+    while i + 63 <= length(seq)
+        # handle the all-N case
+        if seq.ns.chunks[dn + 1] == 0xffffffffffffffff
+            counts.n += 64
+        else
+            counts.a += count_a(seq.data[d + 1]) + count_a(seq.data[d + 2])
+            counts.c += count_c(seq.data[d + 1]) + count_c(seq.data[d + 2])
+            counts.g += count_g(seq.data[d + 1]) + count_g(seq.data[d + 2])
+            counts.t += count_t(seq.data[d + 1]) + count_t(seq.data[d + 2])
+
+            x = seq.ns.chunks[dn + 1]
+            if x != 0
+                for j in 1:64
+                    if x & 0x01 != 0
+                        counts.n += 1
+                        counts[getnuc(T, seq.data, seq.part.start + i + j - 2)] -= 1
+                    end
+
+                    x >>= 1
+                    if x == 0
+                        break
+                    end
+                end
+            end
+        end
+
+        dn += 1
+        d += 2
+        i += 64
     end
 
     # count trailing unaligned bases
     while i <= length(seq)
-        counts[getnuc(T, seq.data, seq.part.start + i - 1)] += 1
+        counts[seq[i]] += 1
         i += 1
-    end
-
-    # process Ns
-    for i in npositions(seq)
-        counts[getnuc(T, seq.data, seq.part.start + i - 1)] -= 1
-        counts[nnucleotide(T)] += 1
     end
 
     return counts
