dff736f920623ebc8faf06ca671be1cc8a3a6aa0
Ward9250 <axolotlfan9250@gmail.com>
2015-06-16 21:00:46 +0100

    Fixed type

--- src/align/align.jl
+++ src/align/align.jl
@@ -4,7 +4,7 @@ using Compat
 using Base.Intrinsics
 
 import Base: convert, getindex, show, length, start, next, done, copy, reverse,
-             show, endof, ==, !=, <, >, <=, >=, 
+             show, endof, ==, !=, <, >, <=, >=
 
 include("operations.jl")
 
