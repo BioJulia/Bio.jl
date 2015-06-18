6bce2e6973d59eec901786ee491ecbdc6a49c8dc
Ward9250 <axolotlfan9250@gmail.com>
2015-06-18 02:44:35 +0100

    Added explicit import of lt from Base.Order

--- src/align/align.jl
+++ src/align/align.jl
@@ -2,10 +2,12 @@ module Align
 
 using Compat
 using Base.Intrinsics
-using Base.Order: Ordering, lt
+using Base.Order: Ordering
 
 import Base: convert, getindex, show, length, start, next, done, copy, reverse,
              show, endof, ==, !=, <, >, <=, >=
+
+import Base.Order: lt
 
 include("operations.jl")
 
