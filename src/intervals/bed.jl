6d10edb82994e99016cd461fbbd9a63237b6fb93
Daniel Jones <dcjones@cs.washington.edu>
2015-06-20 22:28:18 -0700

    Fix BED parser not clearing fields between lines.

--- src/intervals/bed.jl
+++ src/intervals/bed.jl
@@ -105,14 +105,14 @@ function takevalue!(input::BEDParser)
                                     input.block_count, input.block_sizes,
                                     input.block_firsts))
     input.strand = STRAND_NA
-    name = Nullable{ASCIIString}()
-    score = Nullable{Int}()
-    thick_first = Nullable{Int}()
-    thick_last = Nullable{Int};()
-    item_rgb = Nullable{RGB{Float32}};()
-    block_count = Nullable{Int}()
-    block_sizes = Nullable{Vector{Int}}()
-    block_firsts = Nullable{Vector{Int}}()
+    input.name = Nullable{ASCIIString}()
+    input.score = Nullable{Int}()
+    input.thick_first = Nullable{Int}()
+    input.thick_last = Nullable{Int}()
+    input.item_rgb = Nullable{RGB{Float32}}()
+    input.block_count = Nullable{Int}()
+    input.block_sizes = Nullable{Vector{Int}}()
+    input.block_firsts = Nullable{Vector{Int}}()
 
     return value
 end
