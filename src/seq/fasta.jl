8160b82daf7e0b6f476ddece31b2abd679fb0c3a
wildgarden <ajanssen@students.uni-mainz.de>
2015-08-09 12:32:43 +0000

    delegate conversion of nucleotides by calling function show()

--- src/seq/fasta.jl
+++ src/seq/fasta.jl
@@ -47,7 +47,7 @@ function Base.write(io::IO, seqrec::FASTASeqRecord)
     counter = 1
     len = length(seqrec.seq)
     for nt in seqrec.seq
-        write(io, convert(Char, nt))
+        show(io, nt)
         if counter % maxchars == 0 && counter < len
             write(io, "\n")
         end
