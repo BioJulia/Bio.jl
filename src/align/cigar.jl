35bc1edbc52b74b5216859e568c498fe17f36023
Ward9250 <axolotlfan9250@gmail.com>
2015-08-18 01:50:29 +0100

    Rename type alias and yet more tests

--- src/align/cigar.jl
+++ src/align/cigar.jl
@@ -20,9 +20,9 @@ function show(io::IO, cigar::CIGAR)
     write(io, convert(String, cigar))
 end
 
-typealias CIGARString Vector{CIGAR}
+typealias CIGARS Vector{CIGAR}
 
-function convert(::Type{CIGARString}, str::String)
+function convert(::Type{CIGARS}, str::String)
     matches = matchall(r"(\d+)(\w)", str)
     cigarString = Vector{CIGAR}(length(matches))
     @inbounds for i in 1:length(matches)
@@ -32,10 +32,10 @@ function convert(::Type{CIGARString}, str::String)
 end
 
 macro cigar_str(str)
-    return CIGARString(str)
+    return CIGARS(str)
 end
 
-function convert(::Type{String}, cigarString::CIGARString)
+function convert(::Type{String}, cigarString::CIGARS)
     outString = ""
     for cigar in cigarString
         outString *= String(cigar)
@@ -43,15 +43,15 @@ function convert(::Type{String}, cigarString::CIGARString)
     return outString
 end
 
-function show(io::IO, cigarstr::CIGARString)
+function show(io::IO, cigarstr::CIGARS)
     write(io, convert(String, cigarstr))
 end
 
-function writemime(io::IO, ::MIME{symbol("text/plain")}, cs::CIGARString)
+function writemime(io::IO, ::MIME{symbol("text/plain")}, cs::CIGARS)
     show(io, cs)
 end
 
-function *(a::CIGARString, b::CIGARString)
+function *(a::CIGARS, b::CIGARS)
     return [a;b]
 end
 
@@ -59,7 +59,7 @@ function *(a::Operation, n::Int)
     return CIGAR(a, n)
 end
 
-function viewPosition(x::CIGARString, position::Int)
+function viewPosition(x::CIGARS, position::Int)
     nextIdx = start(x)
     currentPos = position
     while !done(x, nextIdx)
--- test/align/TestAlign.jl
+++ test/align/TestAlign.jl
@@ -3,70 +3,110 @@ module TestAlign
 using FactCheck
 using Bio.Align
 
-chars1 = ['-', '=', 'M', 'm', 'N', 'n', 'X', 'x', 'S', 's', 'H', 'h', 'I', 'i',
-         'D', 'd', 'P', 'p']
+## Constants and Parameters used for defining and running the tests.
 
-chars2 = ['-', '=', 'M', 'N', 'X', 'S', 'H', 'I', 'D', 'P']
+const MAX_CIGAR_SIZE = 100000
 
-ops1 = [OP_GAP, OP_MATCH, OP_MM, OP_MM, OP_N, OP_N, OP_MISMATCH, OP_MISMATCH,
-       OP_SCLIP, OP_SCLIP, OP_HCLIP, OP_HCLIP, OP_INSERT, OP_INSERT, OP_DELETE,
-       OP_DELETE, OP_PAD, OP_PAD]
+const NUMBER_OF_RANDOM_CIGAR = 10
 
-ops2 = unique(ops1)
+const MAX_CIGARS_IN_CIGAR_STRING = 50
 
-cigarSize = abs(rand(Int, length(ops2)))
+const NUMBER_OF_RANDOM_CIGAR_STRINGS = 10
 
-strings = ["$(cigarSize[i])$(chars2[i])" for i in 1:length(cigarSize)]
+const ALLOWED_CHARS = ['-', '=', 'M', 'm', 'N', 'n', 'X', 'x', 'S', 's', 'H',
+                       'h', 'I', 'i', 'D', 'd', 'P', 'p']
+
+const CHARS_FROM_OPS = ['-', '=', 'M', 'N', 'X', 'S', 'H', 'I', 'D', 'P']
+
+const OPS_TWICE = [OP_GAP, OP_MATCH, OP_MM, OP_MM, OP_N, OP_N, OP_MISMATCH,
+              OP_MISMATCH, OP_SCLIP, OP_SCLIP, OP_HCLIP, OP_HCLIP, OP_INSERT,
+              OP_INSERT, OP_DELETE, OP_DELETE, OP_PAD, OP_PAD]
+
+const OPS_UNIQUE = unique(OPS_TWICE)
+
+# Need some random CIGAR sizes for some tests.
+const cigarSize = abs(rand(1:MAX_CIGAR_SIZE, length(OPS_UNIQUE)))
+
+strings = ["$(cigarSize[i])$(CHARS_FROM_OPS[i])" for i in 1:length(cigarSize)]
+
+## Functions used for tests.
 
 function cigarsMatch(cigar::CIGAR, op::Operation, size::Int)
     cigar.OP == op && cigar.Size == size
 end
 
-function randomCIGARS()
 
-end
-
+## Test procedure.
 
 facts("Alignments") do
     context("Operations") do
-        context("Conversions") do
+        context("Constructors and Conversions") do
             @inbounds for i in 1:length(chars1)
-                @fact Operation(chars1[i]) --> ops1[i]
+                @fact Operation(chars1[i]) --> OPS_TWICE[i]
             end
-            @inbounds for i in 1:length(ops2)
-                @fact Char(eval(ops2[i])) --> chars2[i]
+            @inbounds for i in 1:length(OPS_UNIQUE)
+                @fact Char(eval(OPS_UNIQUE[i])) --> CHARS_FROM_OPS[i]
             end
         end
 
     end
 
-    context("CIGAR") do
-        context("Constructors") do
+    context("Single CIGARs") do
+        context("Constructors and Conversions") do
             context("From Operation and Size") do
-                @inbounds for i in 1:length(ops2)
-                    @fact cigarsMatch(CIGAR(ops2[i], cigarSize[i]), ops2[i], cigarSize[i]) --> true
+                # Check GICARs are correctly constructed from an Operation and Size argument.
+                @inbounds for i in 1:length(OPS_UNIQUE)
+                    @fact cigarsMatch(CIGAR(OPS_UNIQUE[i], cigarSize[i]), OPS_UNIQUE[i], cigarSize[i]) --> true
                 end
             end
 
             context("From Character and Size") do
-                @inbounds for i in 1:length(chars2)
-                    @fact cigarsMatch(CIGAR(chars2[i], cigarSize[i]), ops2[i], cigarSize[i]) --> true
+                # Check CIGARs are correctly constructed from a character and size argument.
+                @inbounds for i in 1:length(CHARS_FROM_OPS)
+                    @fact cigarsMatch(CIGAR(CHARS_FROM_OPS[i], cigarSize[i]), OPS_UNIQUE[i], cigarSize[i]) --> true
                 end
             end
 
             context("From Strings") do
+                # Check Strings are successfully converted to CIGAR.
                 @inbounds for i in 1:length(strings)
-                    @fact cigarsMatch(CIGAR(strings[i])) --> true
+                    @fact cigarsMatch(CIGAR(strings[i]), OPS_UNIQUE[i], cigarSize[i]) --> true
+                end
             end
 
-
-
-
-
+            context("To Strings from CIGAR") do
+                # Check that CIGAR operations are successfully converted to Strings.
+                @inbounds for i in 1:length(OPS_UNIQUE)
+                    @fact String(CIGAR(OPS_UNIQUE[i], cigarSize[i])) --> "$(cigarSize[i])$(OPS_UNIQUE[i])"
+                end
+            end
         end
     end
 
-    context("Anchors") do
+    context("CIGARS") do
+        context("Constructors and Conversions") do
+            context("From String to CIGARS") do
+                for i in 1:NUMBER_OF_RANDOM_CIGAR_STRINGS
+                    numCigar = rand(1:MAX_CIGARS_IN_CIGAR_STRING)
+                    operationChars = rand(ALLOWED_CHARS, numCigar)
+                    operationOPS = [Operation(i) for i in operationChars]
+                    cigarLengths = rand(1:MAX_CIGAR_SIZE, numCigar)
+                    out = ""
+                    @inbounds for i in 1:MAX_CIGARS_IN_CIGAR_STRING
+                        out *= "$(cigarLengths[i])$(operationChars[i])"
+                    end
+                    cigars = CIGARS(out)
+                    @fact length(cigars) --> numCigar
+                    for i in 1:numCigar
+                        @fact cigars[i].OP --> operationOPS[i]
+                        @fact cigars[i].Size --> cigarLengths[i]
+                    end
+                end
+            end
+        end
+    end
+
+    context("AlignmentAnchor") do
 
     end
 end
