ffb8630213928facf0953f4b6baa742a5bbd7ba3
Ward9250 <axolotlfan9250@gmail.com>
2015-09-29 19:41:39 +0100

    Added generated constructor that guards against duplicate labels

--- src/annotations.jl
+++ src/annotations.jl
@@ -1,7 +1,16 @@
-# Design borrows heavily from DataFrames prototypes for type stable DataFrames.
-# Because the type of a tuple is a tupleof the types, we can exploit this to get
-# some type stability and predictability for a flexible data structure in which types
-# may not be known in advance.
+# Annotations
+# ===========
+
+#=
+Type-stable, yet flexible container. Useful for storing things like phylogeny
+and sequence annotations, without hving to simply accept the speed hit or type
+instability.
+
+Design borrows heavily from DataFrames prototypes for type stable DataFrames.
+Because the type of a tuple is a tupleof the types, we can exploit this to get
+some type stability and predictability for a flexible data structure in which
+types may not be known in advance.
+=#
 
 parameters{T}(::Type{T}) = T.parameters
 
@@ -15,7 +24,18 @@ type Annotations{D <: Tuple, L <: Tuple} <: AbstractAnnotation
     labels::Type{L}
 end
 
-# Type stable construction is possible with a macro:
+
+# Generated function outer constructor prevents duplicate labels for fields.
+@generated function Annotations{D <: Tuple, L <: Tuple}(data::D, labels::Type{L})
+    lparams = parameters(L)
+    if length(unique(lparams)) < length(lparams)
+        return :(error("Trying to give more than one field the same label."))
+    else
+        return :(Annotations{D, L}(data, labels))
+    end
+end
+
+# Type stable construction is made much more easy with a macro:
 
 macro annotations(kwargs...)
     labels = map(i->i.args[1], kwargs)
@@ -23,16 +43,16 @@ macro annotations(kwargs...)
     return :(Annotations($data, Tuple{$(labels...)}))
 end
 
-abstract Annotation{a}
+abstract Field{a}
 
-@generated function Base.getindex{D, L, a}(annotation::Annotations{D, L}, ::Type{Annotation{a}})
+@generated function Base.getindex{D, L, a}(annotation::Annotations{D, L}, ::Type{Field{a}})
     index = findfirst(parameters(L), a)
     return :(annotation.data[$index])
 end
 
 Base.getindex{a}(annotations::Annotations, i, ann::Type{a}) = annotations[field][i]
 
-@generated function Base.setindex!{D, L, A, a}(annotations::Annotations{D, L}, addition::A, ::Type{Annotation{a}})
+@generated function Base.setindex!{D, L, A, a}(annotations::Annotations{D, L}, addition::A, ::Type{Field{a}})
     L2 = Tuple{parameters(L)..., a}
     D2 = Tuple{parameters(D)..., A}
     return :(Annotations{$D2, $L2}(tuple(annotations.data..., addition), $L2))
