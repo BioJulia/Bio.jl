d618f4d4617787a601adf35d2a6321d57e64dae5
Kenta Sato <bicycle1885@gmail.com>
2015-11-04 02:20:52 +0900

    fix Smith-Waterman

--- src/align/pairwise/pairalign.jl
+++ src/align/pairwise/pairalign.jl
@@ -6,6 +6,7 @@ include("result.jl")
 include("algorithms/common.jl")
 include("algorithms/needleman_wunsch.jl")
 include("algorithms/banded_needleman_wunsch.jl")
+include("algorithms/smith_waterman.jl")
 include("algorithms/affinegap_global_align.jl")
 include("algorithms/affinegap_banded_global_align.jl")
 include("algorithms/affinegap_local_align.jl")
@@ -66,17 +67,16 @@ function pairalign{S1,S2,T}(::SemiGlobalAlignment, a::S1, b::S2, score::AffineGa
     end
 end
 
-function pairalign{S1,S2}(::LocalAlignment, a::S1, b::S2, score::AffineGapScoreModel;
+function pairalign{S1,S2,T}(::LocalAlignment, a::S1, b::S2, score::AffineGapScoreModel{T};
                           score_only::Bool=false)
-    submat = score.submat
-    gop = score.gap_open_penalty
-    gep = score.gap_extend_penalty
+    m = length(a)
+    n = length(b)
+    sw = SmithWaterman{T}(m, n)
+    score, endpos = run!(sw, a, b, score.submat, -score.gap_open_penalty, -score.gap_extend_penalty)
     if score_only
-        score, _, _ = affinegap_local_align(a, b, submat, gop, gep)
         return PairwiseAlignment{S1,S2}(score, true)
     else
-        score, trace, best_endpos = affinegap_local_align(a, b, submat, gop, gep)
-        a′ = affine_local_traceback(a, b, trace, best_endpos)
+        a′ = traceback(sw, a, b, endpos)
         return PairwiseAlignment(score, true, a′, b)
     end
 end
