aa72958d2972f4a23a626a62c7a5449b154ff894
Kenta Sato <bicycle1885@gmail.com>
2016-03-01 01:31:54 +0900

    add stop codon and amino acid gap

--- src/seq/aminoacid.jl
+++ src/seq/aminoacid.jl
@@ -29,127 +29,74 @@ Base.isless(x::AminoAcid, y::AminoAcid) = isless(UInt8(x), UInt8(y))
 # Amino acid encoding definition
 # ------------------------------
 
+"Invalid Amino Acid"
+const AA_INVALID = convert(AminoAcid, 0x1c)  # Used during conversion from strings
+
+# lookup table for characters
+const char_to_aa = [AA_INVALID for _ in 0x00:0x7f]
+const aa_to_char = Vector{Char}(0x1c)
+
 # This set of amino acids is defined by IUPAC-IUB Joint Commission on Biochemical Nomenclature.
 # Reference: http://www.insdc.org/documents/feature_table.html#7.4.3
 
-"Alanine"
-const AA_A = convert(AminoAcid, 0x00)
+for (aa, doc, code) in [
+        ('A', "Alanine",                           0x00),
+        ('R', "Arginine",                          0x01),
+        ('N', "Asparagine",                        0x02),
+        ('D', "Aspartic Acid",                     0x03),
+        ('C', "Cysteine",                          0x04),
+        ('Q', "Glutamine",                         0x05),
+        ('E', "Glutamic Acid",                     0x06),
+        ('G', "Glycine",                           0x07),
+        ('H', "Histidine",                         0x08),
+        ('I', "Isoleucine",                        0x09),
+        ('L', "Leucine",                           0x0a),
+        ('K', "Lysine",                            0x0b),
+        ('M', "Methionine",                        0x0c),
+        ('F', "Phenylalanine",                     0x0d),
+        ('P', "Proline",                           0x0e),
+        ('S', "Serine",                            0x0f),
+        ('T', "Threonine",                         0x10),
+        ('W', "Tryptophan",                        0x11),
+        ('Y', "Tyrosine",                          0x12),
+        ('V', "Valine",                            0x13),
+        ('B', "Aspartic Acid or Asparagine",       0x14),  # ambiguous
+        ('J', "Leucine or Isoleucine",             0x15),  # ambiguous
+        ('Z', "Glutamine or Glutamic Acid",        0x16),  # ambiguous
+        ('X', "Unspecified or Unknown Amino Acid", 0x17),  # ambiguous
+        ('O', "Pyrrolysine",                       0x18),  # non-standard
+        ('U', "Selenocysteine",                    0x19)]  # non-standard
+    var = symbol("AA_", aa)
+    @eval begin
+        @doc $doc const $var = convert(AminoAcid, $code)
+        char_to_aa[$(Int(aa)+1)] = char_to_aa[$(Int(lowercase(aa))+1)] = $var
+        aa_to_char[$(code)+1] = $aa
+    end
+end
 
-"Arginine"
-const AA_R = convert(AminoAcid, 0x01)
+"Stop"
+const AA_Stop = convert(AminoAcid, 0x1a)
+char_to_aa[Int('*')+1] = AA_Stop
+aa_to_char[0x1a+1] = '*'
 
-"Asparagine"
-const AA_N = convert(AminoAcid, 0x02)
+"Amino Acid Gap"
+const AA_Gap = convert(AminoAcid, 0x1b)
+char_to_aa[Int('-') + 1] = AA_Gap
+aa_to_char[0x1b+1] = '-'
 
-"Aspartic Acid"
-const AA_D = convert(AminoAcid, 0x03)
-
-"Cysteine"
-const AA_C = convert(AminoAcid, 0x04)
-
-"Glutamine"
-const AA_Q = convert(AminoAcid, 0x05)
-
-"Glutamic Acid"
-const AA_E = convert(AminoAcid, 0x06)
-
-"Glycine"
-const AA_G = convert(AminoAcid, 0x07)
-
-"Histidine"
-const AA_H = convert(AminoAcid, 0x08)
-
-"Isoleucine"
-const AA_I = convert(AminoAcid, 0x09)
-
-"Leucine"
-const AA_L = convert(AminoAcid, 0x0a)
-
-"Lysine"
-const AA_K = convert(AminoAcid, 0x0b)
-
-"Methionine"
-const AA_M = convert(AminoAcid, 0x0c)
-
-"Phenylalanine"
-const AA_F = convert(AminoAcid, 0x0d)
-
-"Proline"
-const AA_P = convert(AminoAcid, 0x0e)
-
-"Serine"
-const AA_S = convert(AminoAcid, 0x0f)
-
-"Threonine"
-const AA_T = convert(AminoAcid, 0x10)
-
-"Tryptophan"
-const AA_W = convert(AminoAcid, 0x11)
-
-"Tyrosine"
-const AA_Y = convert(AminoAcid, 0x12)
-
-"Valine"
-const AA_V = convert(AminoAcid, 0x13)
-
-"Aspartic Acid or Asparagine"  # ambiguous
-const AA_B = convert(AminoAcid, 0x14)
-
-"Leucine or Isoleucine"  # ambiguous
-const AA_J = convert(AminoAcid, 0x15)
-
-"Glutamine or Glutamic Acid"  # ambiguous
-const AA_Z = convert(AminoAcid, 0x16)
-
-"Unspecified or Unknown Amino Acid"  # ambiguous
-const AA_X = convert(AminoAcid, 0x17)
-
-"Pyrrolysine"  # non-standard
-const AA_O = convert(AminoAcid, 0x18)
-
-"Selenocysteine"  # non-standard
-const AA_U = convert(AminoAcid, 0x19)
-
-"Invalid Amino Acid"
-const AA_INVALID = convert(AminoAcid, 0x1a) # Used during conversion from strings
-
-isvalid(aa::AminoAcid) = aa ≤ AA_U
-alphabet(::Type{AminoAcid}) = AA_A:AA_U
-
-Base.isless(x::AminoAcid, y::AminoAcid) = isless(UInt8(x), UInt8(y))
-Base.(:-)(x::AminoAcid, y::AminoAcid) = Int(UInt8(x)) - Int(UInt8(y))
-Base.(:-)(x::AminoAcid, y::Integer) = reinterpret(AminoAcid, Int8(UInt8(x)) - Int8(UInt8(y)))
-Base.(:+)(x::AminoAcid, y::Integer) = reinterpret(AminoAcid, Int8(UInt8(x)) + Int8(y))
-Base.(:+)(x::Integer, y::AminoAcid) = y + x
+isvalid(aa::AminoAcid) = aa ≤ AA_Gap
+alphabet(::Type{AminoAcid}) = AA_A:AA_Gap
 
 
 # Conversion from/to Char
 # -----------------------
 
-# lookup table for characters in 'A':'z'
-const char_to_aa = [
-    AA_A,       AA_B,       AA_C,       AA_D,       AA_E,       AA_F,
-    AA_G,       AA_H,       AA_I,       AA_J,       AA_K,       AA_L,
-    AA_M,       AA_N,       AA_O,       AA_P,       AA_Q,       AA_R,
-    AA_S,       AA_T,       AA_U,       AA_V,       AA_W,       AA_X,
-    AA_Y,       AA_Z,       AA_INVALID, AA_INVALID, AA_INVALID, AA_INVALID,
-    AA_INVALID, AA_INVALID, AA_A,       AA_B,       AA_C,       AA_D,
-    AA_E,       AA_F,       AA_G,       AA_H,       AA_I,       AA_J,
-    AA_K,       AA_L,       AA_M,       AA_N,       AA_O,       AA_P,
-    AA_Q,       AA_R,       AA_S,       AA_T,       AA_U,       AA_V,
-    AA_W,       AA_X,       AA_Y,       AA_Z]
-
 function Base.convert(::Type{AminoAcid}, c::Char)
-    @inbounds aa = 'A' <= c <= 'y' ? char_to_aa[c - 'A' + 1] : AA_INVALID
+    @inbounds aa = c <= '\x7f' ? char_to_aa[Int(c)+1] : AA_INVALID
     @assert aa != AA_INVALID error("$(c) is not a valid amino acid")
     return aa
 end
 
-const aa_to_char = [
-    'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I',
-    'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V',
-    'B', 'J', 'Z', 'X',
-    'O', 'U', ]
 Base.convert(::Type{Char}, aa::AminoAcid) = aa_to_char[convert(UInt8, aa) + 1]
 
 
@@ -160,7 +107,7 @@ function Base.show(io::IO, aa::AminoAcid)
     if aa == AA_INVALID
         write(io, "Invalid Amino Acid")
     else
-        write(io, convert(Char, aa))
+        write(io, Char(aa))
     end
 end
 
