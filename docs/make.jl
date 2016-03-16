f6e3c65469342a969b9c697a3966cd5123d46066
Ward9250 <axolotlfan9250@gmail.com>
2016-03-16 14:09:09 +0000

    Added mkdocs material theme

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,10 @@
 using Lapidary, Bio
 
+custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
 makedocs()
-deploydocs(repo = "github.com/BioJulia/Bio.jl.git", julia = "0.4", osname="linux")
+deploydocs(
+           repo = "github.com/BioJulia/Bio.jl.git",
+           julia = "0.4",
+           osname = "linux",
+           deps = custom_deps
+           )
