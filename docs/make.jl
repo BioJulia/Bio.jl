aec1ea257ede11f0a804f1e265cf39f5b5e5879d
Ward9250 <axolotlfan9250@gmail.com>
2016-03-17 15:19:17 +0000

    moved logo to images folder, as assets folder is reserved for lapidary

--- docs/make.jl
+++ docs/make.jl
@@ -2,10 +2,6 @@ using Lapidary, Bio
 
 custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
 makedocs()
-cp(
-   Pkg.dir("Bio", "docs/src/assets/images/biojl.png"),
-   Pkg.dir("Bio", "docs/build/assets/images/biojl.png")
-   )
 deploydocs(
            repo = "github.com/BioJulia/Bio.jl.git",
            julia = "0.4",
