faf17d3e430a2f6ecc96eab775245f8ef6bc193c
Kevin Bonham, PhD <kevbonham@gmail.com>
2016-04-02 12:15:49 -0400

    add tests for blastn and blastp

--- test/tools/runtests.jl
+++ test/tools/runtests.jl
@@ -1,4 +1,48 @@
 module TestTools
 
+if VERSION >= v"0.5-"
+    using Base.Test
+else
+    using BaseTestNext
+    const Test = BaseTestNext
+end
+
+using Bio.Seq,
+      Bio.Blast,
+      TestFunctions
+
+get_bio_fmt_specimens()
+path = Pkg.dir("Bio", "test", "BioFmtSpecimens")
+
+@testset "Blast+ blastn" begin
+    na1 = dna"CGGACCAGACGGACACAGGGAGAAGCTAGTTTCTTTCATGTGATTGANATNATGACTCTACTCCTAAAAGGGAAAAANCAATATCCTTGTTTACAGAAGAGAAACAAACAAGCCCCACTCAGCTCAGTCACAGGAGAGAN"
+    na2 = dna"CGGAGCCAGCGAGCATATGCTGCATGAGGACCTTTCTATCTTACATTATGGCTGGGAATCTTACTCTTTCATCTGATACCTTGTTCAGATTTCAAAATAGTTGTAGCCTTATCCTGGTTTTACAGATGTGAAACTTTCAA"
+    fna = joinpath(path, "FASTA", "f002.fasta")
+    nucldb = joinpath(path, "BLASTDB", "f002")
+    nuclresults = joinpath(path, "BLASTDB", "f002.xml")
+
+    @test blastn(na1, na2)
+    @test blastn(na1, [na1, na2])
+    @test blastn([na1, na2], [na1, na2])
+    @test blastn(na1, nucldb, db=true)
+    @test blastn(na1, fna)
+    @test blastn(fna, nucldb, db=true)
+
+end
+
+@testset "Blast+ blastp" begin
+    aa1 = aa"MWATLPLLCAGAWLLGVPVCGAAELSVNSLEKFHFKSWMSKHRKTYSTEEYHHRLQTFASNWRKINAHNNGNHTFKMALNQFSDMSFAEIKHKYLWSEPQNCSATKSNYLRGTGPYPPSVDWRKKGNFVSPVKNQGACGS"
+    aa2 = aa"MWTALPLLCAGAWLLSAGATAELTVNAIEKFHFTSWMKQHQKTYSSREYSHRLQVFANNWRKIQAHNQRNHTFKMGLNQFSDMSFAEIKHKYLWSEPQNCSATKSNYLRGTGPYPSSMDWRKKGNVVSPVKNQGACGSCW"
+    faa = joinpath(path, "FASTA", "cysprot.fasta")
+    protdb = joinpath(path, "BLASTDB", "cysprot")
+    protresults = joinpath(path, "BLASTDB", "cysprot.xml")
+
+    @test blastp(aa1, aa2)
+    @test blastp(aa1, [aa1, aa2])
+    @test blastp([aa1, aa2], [aa1, aa2])
+    @test blastp(aa1, protdb, db=true)
+    @test blastp(aa1, faa)
+    @test blastp(faa, protdb, db=true)
+end
 
 end # TestTools
