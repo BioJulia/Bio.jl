3fbc093324515bf996fe15844a82a654710e6329
Ward9250 <axolotlfan9250@gmail.com>
2016-04-07 22:56:40 +0100

    Added test cases for constructors

--- test/phylo/runtests.jl
+++ test/phylo/runtests.jl
@@ -16,6 +16,38 @@ using Bio.Phylo
             @test has_branchlength(node) == false
             @test has_support(node) == false
             @test isempty(node) == true
+            @test node.name == ""
+            @test typeof(nometa_one) == PhyNode{Void, Void, Void}
+        end
+        @testset "Named nodes" begin
+            named = PhyNode("MRCA")
+            @test has_branchlength(named) == false
+            @test has_support(named) == false
+            @test isempty(named) == true
+            @test named.name == "MRCA"
+            @test typeof(nometa_one) == PhyNode{Void, Void, Void}
+        end
+        @testset "Nodes with support and branch" begin
+            nometa_one = PhyNode("MRCA1", 0.342, 0.982)
+            @test has_branchlength(nometa_one) == true
+            @test has_support(nometa_one) == true
+            @test isempty(nometa_one) == false
+            @test nometa_one.name == "MRCA1"
+            @test typeof(nometa_one) == PhyNode{Float, Float, Void}
+
+            nometa_two = PhyNode("MRCA2", true, 0.982)
+            @test has_branchlength(nometa_two) == true
+            @test has_support(nometa_two) == true
+            @test isempty(nometa_two) == false
+            @test nometa_two.name == "MRCA2"
+            @test typeof(nometa_two) == PhyNode{Bool, Float, Void}
+
+            nometa_three = PhyNode("MRCA3", 'A', nothing)
+            @test has_branchlength(nometa_three) == false
+            @test has_support(nometa_three) == false
+            @test isempty(nometa_three) == true
+            @test nometa_three.name == "MRCA3"
+            @test typeof(nometa_three) == PhyNode{Char, Void, Void}
         end
     end
     @testset "Basic node fields" begin
