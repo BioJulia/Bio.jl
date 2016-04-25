c16b7cad1cca52783029b6c0276e410c06b8a3f7
Ward9250 <axolotlfan9250@gmail.com>
2016-04-25 20:35:38 +0100

    Added first batch of tests for SingleIndex

--- test/util/indexing.jl
+++ test/util/indexing.jl
@@ -9,7 +9,53 @@ end
 
 using Bio.Indexing
 
+@testset "SingleIndex" begin
+    symbolnames = [:First, :Second, :Third, :Fourth, :Fifth]
+    textnames = ["First", "Second", "Third", "Fourth", "Fifth"]
+    i = SingleIndex(symbolnames)
+    e = SingleIndex()
+    t = SingleIndex(textnames)
+    @testset "Construction" begin
+        @test e == SingleIndex()
+        @test t != SingleIndex()
+        @test i != SingleIndex()
+        @test i == SingleIndex(convert(Vector{Symbol}, symbolnames))
+        @test t == i
+    end
+    @testset "Basic Operators" begin
+        @test length(i) == 5
+        @test length(e) == 0
+        @test length(t) == 5
+        @test names(i) == symbolnames
+        @test names(t) == symbolnames
+        @test names(e) == Symbol[]
+        @test isequal(copy(i), i) && !is(copy(i), i)
+        @test isequal(copy(e), e) && !is(copy(e), e)
+        @test isequal(copy(t), t) && !is(copy(t), t)
+        for n in symbolnames
+            @test haskey(i, n) == true
+            @test haskey(t, n) == true
+            @test haskey(e, n) == false
+        end
+        for n in textnames
+            @test haskey(i, n) == true
+            @test haskey(t, n) == true
+            @test haskey(e, n) == false
+        end
+        for n in 1:5
+            @test haskey(i, n) == true
+            @test haskey(t, n) == true
+            @test haskey(e, n) == false
+        end
+        @test keys(i) == symbolnames
+        @test keys(t) == symbolnames
+        @test keys(e) != symbolnames
+        @test keys(e) == Symbol[]
+    end
 
+
+
+end
 
 
 
