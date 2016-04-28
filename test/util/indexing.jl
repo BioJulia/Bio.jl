e0265da941f6737f09402504fb7898d2b54bd75e
Ward9250 <axolotlfan9250@gmail.com>
2016-04-28 18:30:59 +0100

    Added brackets to rename tests

--- test/util/indexing.jl
+++ test/util/indexing.jl
@@ -59,16 +59,16 @@ using Bio.Indexing
             newnames = [:Sixth, :Seventh, :Eighth, :Ninth, :Tenth]
             newnamestext = ["Sixth", "Seventh", "Eighth", "Ninth", "Tenth"]
             @test names!(i, newnames) == SingleIndex(newnames)
-            @test_throws names!(i, newnames[1:4])
+            @test_throws ArgumentError names!(i, newnames[1:4])
             @test names!(i, newnamestext) == SingleIndex(newnamestext)
-            @test_throws names!(i, newnamestext[1:4])
+            @test_throws ArgumentError names!(i, newnamestext[1:4])
 
             # Now let's manipulate some of those names by renaming them
 
             # Make a new index by replacing name Sixth with First,
             # and test that only :Sixth is different
             rn = rename(i, :Sixth, :First)
-            @test rn != SingleIndex(symbolnames) && rn != SingleIndex(newnames)
+            @test (rn != SingleIndex(symbolnames)) && (rn != SingleIndex(newnames))
             @test rn.names[1] == :First
             @test rn.names[2] == :Seventh
             @test rn.names[3] == :Eighth
@@ -78,31 +78,31 @@ using Bio.Indexing
             # Let's make a new index by replacing the names, Sixth Eights, and
             # Tenth, with First, Third, and Fifth.
             rntwo = rename(i, [:Sixth, :Eighth, :Tenth], [:First, :Third, :Fifth])
-            @test rntwo != SingleIndex(symbolnames) && rntwo != SingleIndex(newnames)
+            @test (rntwo != SingleIndex(symbolnames)) && (rntwo != SingleIndex(newnames))
             @test rn != rntwo
 
             # Now let's test that renaming worked correctly, some names should
             # match names in symbolnames, and some should match names in
             # newnames.
-            @test rntwo.names[1] == newnames[1] && rntwo.names[1] != symbolnames[1]
-            @test rntwo.names[2] != newnames[2] && rntwo.names[2] == symbolnames[2]
-            @test rntwo.names[3] == newnames[3] && rntwo.names[3] != symbolnames[3]
-            @test rntwo.names[4] != newnames[4] && rntwo.names[4] == symbolnames[4]
-            @test rntwo.names[5] == newnames[5] && rntwo.names[5] != symbolnames[5]
+            @test (rntwo.names[1] != newnames[1]) && (rntwo.names[1] == symbolnames[1])
+            @test (rntwo.names[2] == newnames[2]) && (rntwo.names[2] != symbolnames[2])
+            @test (rntwo.names[3] != newnames[3]) && (rntwo.names[3] == symbolnames[3])
+            @test (rntwo.names[4] == newnames[4]) && (rntwo.names[4] != symbolnames[4])
+            @test (rntwo.names[5] != newnames[5]) && (rntwo.names[5] == symbolnames[5])
 
-            @test rntwo.names[1] != i.names[1] && rntwo.names[1] == t.names[1]
-            @test rntwo.names[2] == i.names[2] && rntwo.names[2] != t.names[2]
-            @test rntwo.names[3] != i.names[3] && rntwo.names[3] == t.names[3]
-            @test rntwo.names[4] == i.names[4] && rntwo.names[4] != t.names[4]
-            @test rntwo.names[5] != i.names[5] && rntwo.names[5] == t.names[5]
+            @test (rntwo.names[1] != i.names[1]) && (rntwo.names[1] == t.names[1])
+            @test (rntwo.names[2] == i.names[2]) && (rntwo.names[2] != t.names[2])
+            @test (rntwo.names[3] != i.names[3]) && (rntwo.names[3] == t.names[3])
+            @test (rntwo.names[4] == i.names[4]) && (rntwo.names[4] != t.names[4])
+            @test (rntwo.names[5] != i.names[5]) && (rntwo.names[5] == t.names[5])
         end
         @testset "getindex" begin
             @test i[1] == 1
             @test i[[true, false, false, true, true]] == [1, 4, 5]
             @test i[1:3] == [1:3;]
-            @test i[:Sixth] == 1 && i[:Tenth] == 5 && i[:Eighth] == 3
-            @test i["Sixth"] == 1 && i["Tenth"] == 5 && i["Eighth"] == 3
-            @test i[[1,3,5]] = [1,3,5]
+            @test (i[:Sixth] == 1) && (i[:Tenth] == 5) && (i[:Eighth] == 3)
+            @test (i["Sixth"] == 1) && (i["Tenth"] == 5) && (i["Eighth"] == 3)
+            @test i[[1,3,5]] == [1,3,5]
             @test i[[:Seventh, :Eighth, :Ninth]] == [2, 3, 4]
             @test i[["Seventh", "Eighth", "Ninth"]] == [2, 3, 4]
         end
