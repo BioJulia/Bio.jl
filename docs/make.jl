6a0c1271bab53dcc19f981f6ebf29eae1c344ff9
Ben J. Ward <axolotlfan9250@gmail.com>
2016-05-12 22:30:55 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 using Documenter, Bio
 
 custom_deps() = run(`pip install --user pygments mkdocs mkdocs-material`)
-@osx? makedocs() : makedocs(doctest = false)
+@osx? makedocs(doctest = false) : makedocs()
 deploydocs(
            repo = "github.com/BioJulia/Bio.jl.git",
            julia = "0.4",
