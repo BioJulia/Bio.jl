3ad22d8af45b0dec22ba913eee2ee9f2efd1146b
Kenta Sato (佐藤 建太) <bicycle1885@gmail.com>
2016-05-24 04:38:40 +0900

    rename Blast to BLAST (take 2) (#199)

--- test/tools/runtests.jl
+++ test/tools/runtests.jl
@@ -14,7 +14,7 @@ using Bio.Seq,
 get_bio_fmt_specimens()
 path = Pkg.dir("Bio", "test", "BioFmtSpecimens")
 
-@testset "Blast+ blastn" begin
+@testset "BLAST+ blastn" begin
     na1 = dna"""
     CGGACCAGACGGACACAGGGAGAAGCTAGTTTCTTTCATGTGATTGANAT
     NATGACTCTACTCCTAAAAGGGAAAAANCAATATCCTTGTTTACAGAAGA
@@ -29,16 +29,16 @@ path = Pkg.dir("Bio", "test", "BioFmtSpecimens")
     nucldb = joinpath(path, "BLASTDB", "f002")
     nuclresults = joinpath(path, "BLASTDB", "f002.xml")
 
-    @test typeof(blastn(na1, na2)) == Array{BlastResult, 1}
-    @test typeof(blastn(na1, [na1, na2])) == Array{BlastResult, 1}
-    @test typeof(blastn([na1, na2], [na1, na2])) == Array{BlastResult, 1}
-    @test typeof(blastn(na1, nucldb, db=true)) == Array{BlastResult, 1}
-    @test typeof(blastn(na1, fna)) == Array{BlastResult, 1}
-    @test typeof(blastn(fna, nucldb, db=true)) == Array{BlastResult, 1}
+    @test typeof(blastn(na1, na2)) == Array{BLASTResult, 1}
+    @test typeof(blastn(na1, [na1, na2])) == Array{BLASTResult, 1}
+    @test typeof(blastn([na1, na2], [na1, na2])) == Array{BLASTResult, 1}
+    @test typeof(blastn(na1, nucldb, db=true)) == Array{BLASTResult, 1}
+    @test typeof(blastn(na1, fna)) == Array{BLASTResult, 1}
+    @test typeof(blastn(fna, nucldb, db=true)) == Array{BLASTResult, 1}
 
 end
 
-@testset "Blast+ blastp" begin
+@testset "BLAST+ blastp" begin
     aa1 = aa"""
     MWATLPLLCAGAWLLGVPVCGAAELSVNSLEKFHFKSWMSKHRKTYSTEE
     YHHRLQTFASNWRKINAHNNGNHTFKMALNQFSDMSFAEIKHKYLWSEPQ
@@ -53,12 +53,12 @@ end
     protdb = joinpath(path, "BLASTDB", "cysprot")
     protresults = joinpath(path, "BLASTDB", "cysprot.xml")
 
-    @test typeof(blastp(aa1, aa2)) == Array{BlastResult, 1}
-    @test typeof(blastp(aa1, [aa1, aa2])) == Array{BlastResult, 1}
-    @test typeof(blastp([aa1, aa2], [aa1, aa2])) == Array{BlastResult, 1}
-    @test typeof(blastp(aa1, protdb, db=true)) == Array{BlastResult, 1}
-    @test typeof(blastp(aa1, faa)) == Array{BlastResult, 1}
-    @test typeof(blastp(faa, protdb, db=true)) == Array{BlastResult, 1}
+    @test typeof(blastp(aa1, aa2)) == Array{BLASTResult, 1}
+    @test typeof(blastp(aa1, [aa1, aa2])) == Array{BLASTResult, 1}
+    @test typeof(blastp([aa1, aa2], [aa1, aa2])) == Array{BLASTResult, 1}
+    @test typeof(blastp(aa1, protdb, db=true)) == Array{BLASTResult, 1}
+    @test typeof(blastp(aa1, faa)) == Array{BLASTResult, 1}
+    @test typeof(blastp(faa, protdb, db=true)) == Array{BLASTResult, 1}
 end
 
 end # TestTools
