0e8c6f72c9ff71ed41eb95c4facfc7e8afa2b63c
Kenta Sato (佐藤 建太) <bicycle1885@gmail.com>
2016-05-24 19:21:30 +0900

    define eltype for KmerNeighborIterator (and add tests) (#203)

--- test/seq/runtests.jl
+++ test/seq/runtests.jl
@@ -1861,6 +1861,17 @@ end
             test_kmer_count(RNAAlphabet{2}, random_rna(len, probs), k, step)
         end
     end
+
+    @testset "De Bruijn Neighbors" begin
+        @test collect(neighbors(DNAKmer("ACG")))  ==
+            DNAKmer["CGA",  "CGC",  "CGG",  "CGT" ]
+        @test collect(neighbors(DNAKmer("GGGG"))) ==
+            DNAKmer["GGGA", "GGGC", "GGGG", "GGGT"]
+        @test collect(neighbors(RNAKmer("ACG")))  ==
+            RNAKmer["CGA",  "CGC",  "CGG",  "CGU" ]
+        @test collect(neighbors(RNAKmer("GGGG"))) ==
+            RNAKmer["GGGA", "GGGC", "GGGG", "GGGU"]
+    end
 end
 
 @testset "Search" begin
