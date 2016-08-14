1b4eb9347b4f2f3dec5373a5b8338ef5ae1059c4
Kenta Sato <bicycle1885@gmail.com>
2016-08-15 02:37:26 +0900

    export SequenceGenerator

--- src/seq/Seq.jl
+++ src/seq/Seq.jl
@@ -78,6 +78,7 @@ export
     npositions,
     hasn,
     gc_content,
+    SequenceGenerator,
     randdnaseq,
     randrnaseq,
     randaaseq,
--- src/seq/randseq.jl
+++ src/seq/randseq.jl
@@ -6,7 +6,9 @@
 # This file is a part of BioJulia.
 # License is MIT: https://github.com/BioJulia/Bio.jl/blob/master/LICENSE.md
 
-# abstract sequence generator type
+"""
+Abstract sequence generator type.
+"""
 abstract SequenceGenerator{T}
 
 # Sequence generator of stationary distributions.
