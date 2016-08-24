a9bed370a18d5f347599b1de39a2cc72e2b4f267
Ben J. Ward <axolotlfan9250@gmail.com>
2016-08-24 18:11:29 +0100

    Corrected my moronic mistake

--- src/seq/composition.jl
+++ src/seq/composition.jl
@@ -119,7 +119,7 @@ end
 function mostfrequent{T}(comp::Composition{T})
     m = max(comp.counts)
     maxes = findin(comp.counts, m)
-    nucs = Vector{T}(length(i))
+    nucs = Vector{T}(length(maxes))
     @inbounds for i in 1:length(nucs)
         nucs[i] = reinterpret(T, maxes[i] - 1)
     end
