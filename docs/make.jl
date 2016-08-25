f6e5732a5e8f8f4a6dc781efc1d2c2254d115b75
Kenta Sato (佐藤 建太) <bicycle1885@gmail.com>
2016-08-26 01:34:45 +0900

    fix Bio.Util docs (#281)

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,10 @@
 using Documenter, Bio
 
 custom_deps() = run(`pip install --user pygments mkdocs mkdocs-biojulia`)
-@osx? makedocs(doctest = false) : makedocs()
+makedocs()
 deploydocs(
            repo = "github.com/BioJulia/Bio.jl.git",
-           julia = "0.4",
+           julia = "0.5",
            osname = "linux",
            deps = custom_deps
            )
