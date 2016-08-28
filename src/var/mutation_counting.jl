94a3a764cfb20d894e57c8728df2e1b9fe039eaa
Ward9250 <axolotlfan9250@gmail.com>
2016-08-29 00:23:10 +0100

    Added count_mutations for both transitions and transversions together

--- src/var/mutation_counting.jl
+++ src/var/mutation_counting.jl
@@ -72,6 +72,44 @@ function count_mutations{A<:NucleotideAlphabets,T<:MutationType}(sequences::Vect
     return nmutations, lengths
 end
 
+function count_mutations{A<:NucleotideAlphabets}(sequences::Vector{BioSequence{A}}, ::Type{TransitionMutation}, ::Type{TransversionMutation})
+    # This method has been written with the aim of improving performance by taking
+    # advantage of the memory layout of matrices of nucleotides, as well as
+    # getting julia to emit simd code for the innermost loop.
+    seqs = seqmatrix(sequences, :seq)
+    S, N = size(seqs)
+    c = binomial(N, 2)
+    lengths = Vector{Int}(c)
+    ntransition = Vector{Int}(c)
+    ntransversion = Vector{Int}(c)
+    target = 1
+    @inbounds for i1 in 1:N
+        for i2 in i1+1:N
+            L = S
+            Ns = 0
+            Nv = 0
+            for s in 1:S
+                s1 = seqs[(i1 - 1) * S + s]
+                s2 = seqs[(i2 - 1) * S + s]
+                isamb = is_ambiguous_strict(s1, s2)
+                isdiff = s1 != s2
+                istrans = (ispurine(a) & ispurine(b)) | (ispyrimidine(a) & ispyrimidine(b))
+                L -= isamb
+                Ns += !isamb & isdiff & istrans
+                Nv += !isamb & isdiff & !istrans
+            end
+            lengths[target] = L
+            ntransition[target] = Ns
+            ntransversion[target] = Nv
+            target += 1
+        end
+    end
+    return ntransition, ntransversion, lengths
+end
+
+function count_mutations{A<:NucleotideAlphabets}(sequences::Vector{BioSequence{A}}, ::Type{TransversionMutation}, ::Type{TransitionMutation})
+    return count_mutations(sequences, TransitionMutation, TransversionMutation)
+end
 
 """
     count_mutations(seq1::BioSequence{A}, seq2::BioSequence{A}, t::Type{DifferentMutation})
