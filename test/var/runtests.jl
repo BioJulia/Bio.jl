332f5e84a884c72761e9df9e5cc2100ac2bbd92c
Ben J. Ward <axolotlfan9250@gmail.com>
2016-09-01 17:11:03 +0100

    Added tests for distance(Matrix{Nucleotide})

--- test/var/runtests.jl
+++ test/var/runtests.jl
@@ -30,34 +30,55 @@ end
 @testset "Distance Computation" begin
 
     dnas1 = [dna"ATTG-ACCTGGNTTTCCGAA", dna"A-ACAGAGTATACRGTCGTC"]
+    m1 = seqmatrix(dnas1, :seq)
 
     dnas2 = [dna"attgaacctggntttccgaa", dna"atacagagtatacrgtcgtc"]
+    m2 = seqmatrix(dnas2, :seq)
 
     @test distance(Count{AnyMutation}, dnas1) == ([12], [16])
     @test distance(Count{TransitionMutation}, dnas1) == ([4], [16])
     @test distance(Count{TransversionMutation}, dnas1) == ([8], [16])
     @test distance(Count{Kimura80}, dnas1) == ([4], [8], [16])
+    @test distance(Count{AnyMutation}, m1) == ([12], [16])
+    @test distance(Count{TransitionMutation}, m1) == ([4], [16])
+    @test distance(Count{TransversionMutation}, m1) == ([8], [16])
+    @test distance(Count{Kimura80}, m1) == ([4], [8], [16])
 
     @test distance(Count{AnyMutation}, dnas2) == ([12], [18])
     @test distance(Count{TransitionMutation}, dnas2) == ([4], [18])
     @test distance(Count{TransversionMutation}, dnas2) == ([8], [18])
     @test distance(Count{Kimura80}, dnas2) == ([4], [8], [18])
+    @test distance(Count{AnyMutation}, m2) == ([12], [18])
+    @test distance(Count{TransitionMutation}, m2) == ([4], [18])
+    @test distance(Count{TransversionMutation}, m2) == ([8], [18])
+    @test distance(Count{Kimura80}, m2) == ([4], [8], [18])
 
     @test distance(Proportion{AnyMutation}, dnas1) == ([(12 / 16)], [16])
     @test distance(Proportion{TransitionMutation}, dnas1) == ([(4 / 16)], [16])
     @test distance(Proportion{TransversionMutation}, dnas1) == ([(8 / 16)], [16])
+    @test distance(Proportion{AnyMutation}, m1) == ([(12 / 16)], [16])
+    @test distance(Proportion{TransitionMutation}, m1) == ([(4 / 16)], [16])
+    @test distance(Proportion{TransversionMutation}, m1) == ([(8 / 16)], [16])
 
     @test distance(Proportion{AnyMutation}, dnas2) == ([(12 / 18)], [18])
     @test distance(Proportion{TransitionMutation}, dnas2) == ([(4 / 18)], [18])
     @test distance(Proportion{TransversionMutation}, dnas2) == ([(8 / 18)], [18])
+    @test distance(Proportion{AnyMutation}, m2) == ([(12 / 18)], [18])
+    @test distance(Proportion{TransitionMutation}, m2) == ([(4 / 18)], [18])
+    @test distance(Proportion{TransversionMutation}, m2) == ([(8 / 18)], [18])
 
     @test distance(JukesCantor69, dnas1) == ([Inf], [Inf]) # Returns infinity as 12/16 is 0.75 - mutation saturation.
+    @test distance(JukesCantor69, m1) == ([Inf], [Inf])
 
     @test round(distance(JukesCantor69, dnas2)[1][1], 3) == 1.648
     @test round(distance(JukesCantor69, dnas2)[2][1], 3) == 1
+    @test round(distance(JukesCantor69, m2)[1][1], 3) == 1.648
+    @test round(distance(JukesCantor69, m2)[2][1], 3) == 1
 
     @test round(distance(Kimura80, dnas2)[1][1], 3) == 1.648
     @test round(distance(Kimura80, dnas2)[2][1], 3) == 1
+    @test round(distance(Kimura80, m2)[1][1], 3) == 1.648
+    @test round(distance(Kimura80, m2)[2][1], 3) == 1
 
 end
 
