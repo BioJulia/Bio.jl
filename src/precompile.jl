34d1b0af6a33d22ec41f4e44b2a503140d24cbef
Kenta Sato <bicycle1885@gmail.com>
2016-09-02 08:39:24 +0900

    remove precompile.jl

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,15 +1,2 @@
 # Precompile
 # ==========
-
-# Reader
-# ------
-
-precompile(Base.open, (String, Type{Seq.FASTA},))
-precompile(Base.open, (String, Type{Seq.FASTQ},))
-precompile(Base.open, (String, Type{Intervals.BED},))
-precompile(Base.read, (Seq.FASTAReader{Seq.BioSequence},))
-precompile(Base.read, (Seq.FASTAReader{Seq.DNASequence},))
-precompile(Base.read, (Seq.FASTAReader{Seq.RNASequence},))
-precompile(Base.read, (Seq.FASTAReader{Seq.AminoAcidSequence},))
-precompile(Base.read, (Seq.FASTQReader{Seq.DNASequence},))
-precompile(Base.read, (Intervals.BEDReader,))
