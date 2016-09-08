08feeb634b766ce28e46de774f17854d15fc52e8
Kenta Sato <bicycle1885@gmail.com>
2016-09-08 17:39:31 +0900

    clean up SAM/BAMRecord printing

--- src/align/hts/hts.jl
+++ src/align/hts/hts.jl
@@ -14,3 +14,32 @@ import BufferedStreams: BufferedInputStream
 
 include("sam/sam.jl")
 include("bam/bam.jl")
+
+function Base.show(io::IO, rec::Union{SAMRecord,BAMRecord})
+    name = seqname(rec)
+    if isempty(name)
+        name = "(empty name)"
+    end
+    if get(io, :compact, false)
+        if ismapped(rec)
+            range = string(refname(rec), ':', leftposition(rec), '-', rightposition(rec))
+            print(io, name, "\t$(range)\t", cigar(rec))
+        else
+            print(io, name, "\tunmapped")
+        end
+    else
+        println(io, summary(rec), ':')
+        println(io, "  sequence name: ", name)
+        println(io, "  reference name: ", refname(rec))
+        println(io, "  leftmost position: ", leftposition(rec))
+        println(io, "  next reference name: ", nextrefname(rec))
+        println(io, "  next leftmost position: ", nextleftposition(rec))
+        println(io, "  mapping quality: ", mappingquality(rec))
+        println(io, "  flag: ", flag(rec))
+        println(io, "  template length: ", templatelength(rec))
+        println(io, "  CIGAR string: ", cigar(rec))
+        println(io, "  sequence: ", String(sequence(rec)))
+        println(io, "  base qualities: ", qualities(rec))
+          print(io, "  optional fields: ", optional_fields(rec))
+    end
+end
