fde871237fcd57bc9f5054bb8c96ba2897ccf956
Ben J. Ward <ward9250@gmail.com>
2016-10-14 18:06:59 +0100

    Fix of Non-standard string literals (#349)
    * Initial commit
    
    * Removed return
    
    * Incorporated both static and dynamic behaviour into the non-standard string literals
    
    * Added overloaded x_str macros to provide origional behaviour if a flag is not specified
    
    * Added an explanation of the difference between static and dynamic allocation when using non-standard string literals
    
    * Edited sequence literals man
    
    * Corrected sequence literal documentation

--- src/seq/bioseq.jl
+++ src/seq/bioseq.jl
@@ -63,19 +63,63 @@ typealias CharSequence      BioSequence{CharAlphabet}
 
 remove_newlines(s) = replace(s, r"\r|\n", "")
 
-macro dna_str(seq, flags...)
+macro dna_str(seq, flag)
+    if flag == "s"
+        return DNASequence(remove_newlines(seq))
+    elseif flag == "d"
+        return quote
+            DNASequence($(remove_newlines(seq)))
+        end
+    end
+    error("Invalid DNA flag: '$(flag)'")
+end
+
+macro dna_str(seq)
     return DNASequence(remove_newlines(seq))
 end
 
-macro rna_str(seq, flags...)
+macro rna_str(seq, flag)
+    if flag == "s"
+        return RNASequence(remove_newlines(seq))
+    elseif flag == "d"
+        return quote
+            RNASequence($(remove_newlines(seq)))
+        end
+    end
+    error("Invalid RNA flag: '$(flag)'")
+end
+
+macro rna_str(seq)
     return RNASequence(remove_newlines(seq))
 end
 
-macro aa_str(seq, flags...)
+macro aa_str(seq, flag)
+    if flag == "s"
+        return AminoAcidSequence(remove_newlines(seq))
+    elseif flag == "d"
+        return quote
+            AminoAcidSequence($(remove_newlines(seq)))
+        end
+    end
+    error("Invalid Amino Acid flag: '$(flag)'")
+end
+
+macro aa_str(seq)
     return AminoAcidSequence(remove_newlines(seq))
 end
 
-macro char_str(seq, flags...)
+macro char_str(seq, flag)
+    if flag == "s"
+        return CharSequence(remove_newlines(seq))
+    elseif flag == "d"
+        return quote
+            CharSequence($(remove_newlines(seq)))
+        end
+    end
+    error("Invalid Char flag: '$(flag)'")
+end
+
+macro char_str(seq)
     return CharSequence(remove_newlines(seq))
 end
 
