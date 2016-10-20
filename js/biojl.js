eae45e4e62c61d8f8f1bc506ec0dbc436f1ffd0c
Ben J. Ward <ward9250@gmail.com>
2016-10-20 23:59:54 +0100

    Update biojl.js

--- js/biojl.js
+++ js/biojl.js
@@ -8,5 +8,5 @@ $("#seqicon").attr('src', 'img/module_icons/Biojl_Seq_Icon_' + getRandomColour()
 $("#alignicon").attr('src', 'img/module_icons/Biojl_Align_Icon_' + getRandomColour() + '.svg');
 $("#intervalsicon").attr('src', 'img/module_icons/Biojl_Intervals_Icon_' + getRandomColour() + '.svg');
 $("#structicon").attr('src', 'img/module_icons/Biojl_Struct_Icon_' + getRandomColour() + '.svg');
-$("#varicon").attr('src', 'img/module_icons/Biojl_Var_Icon_' + getRandomColour() + '.svg');
+$("#varicon").attr('src', 'img/module_icons/BioJl_Var_Icon_' + getRandomColour() + '.svg');
 $("#phyloicon").attr('src', 'img/module_icons/Biojl_Phylo_Icon_' + getRandomColour() + '.svg');
