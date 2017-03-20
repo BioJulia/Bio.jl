907585c84e894842881cb49185f9d6def04ed35a
Kenta Sato <bicycle1885@gmail.com>
2017-03-20 23:31:50 +0900

    seqname_isless

--- src/intervals/overlap.jl
+++ src/intervals/overlap.jl
@@ -1,30 +1,34 @@
 # Overlap Iterator
 # ================
 
-immutable OverlapIterator{Sa,Sb}
+immutable OverlapIterator{Sa,Sb,F}
     intervals_a::Sa
     intervals_b::Sb
+    isless::F
 end
 
-function Base.eltype{Sa,Sb}(::Type{OverlapIterator{Sa,Sb}})
+function Base.eltype{Sa,Sb,F}(::Type{OverlapIterator{Sa,Sb,F}})
     return Tuple{Interval{metadatatype(Sa)},Interval{metadatatype(Sb)}}
 end
 
-function Base.iteratorsize{Sa,Sb}(::Type{OverlapIterator{Sa,Sb}})
+function Base.iteratorsize{Sa,Sb,F}(::Type{OverlapIterator{Sa,Sb,F}})
     return Base.SizeUnknown()
 end
 
 """
-    eachoverlap(intervals_a, intervals_b)
+    eachoverlap(intervals_a, intervals_b, [seqname_isless=Base.isless])
 
 Create an iterator of overlapping intervals between `intervals_a` and `intervals_b`.
 
 This function assumes elements of `intervals_a` and `intervals_b` are sorted by
 its sequence name and left position.  If the element type is not a subtype of
 `Bio.Intervals.Interval`, elements are converted to `Interval` objects.
+
+The third argument is a function that defines the order of sequence names. The
+default function is `Base.isless`, which is the lexicographical order.
 """
-function eachoverlap(intervals_a, intervals_b)
-    return OverlapIterator(intervals_a, intervals_b)
+function eachoverlap(intervals_a, intervals_b, seqname_isless=Base.isless)
+    return OverlapIterator(intervals_a, intervals_b, seqname_isless)
 end
 
 type OverlapIteratorState{Sa,Sb,Ta,Tb}
@@ -93,7 +97,7 @@ function advance!(state::OverlapIteratorState, iter::OverlapIterator)
 
     # check i1 and i2 are ordered
     function check_ordered(i1, i2)
-        if !Bio.Intervals.isordered(i1, i2)
+        if !Bio.Intervals.isordered(i1, i2, iter.isless)
             error("intervals are not sorted")
         end
         return nothing
@@ -107,7 +111,7 @@ function advance!(state::OverlapIteratorState, iter::OverlapIterator)
     end
     while !done(state.queue, state.state_queue)
         interval_b, next_state_queue = next(state.queue, state.state_queue)
-        c = compare_overlap(state.interval_a, interval_b)
+        c = compare_overlap(state.interval_a, interval_b, iter.isless)
         if c < 0
             if done(iter.intervals_a, state.state_a)
                 break
@@ -136,9 +140,29 @@ function advance!(state::OverlapIteratorState, iter::OverlapIterator)
     return state
 end
 
-# Retrun -1 when `i1` precedes `i2`, 0 when `i1` overlaps with `i2`, and +1 when
-# `i1` follows `i2`.
-function compare_overlap(i1::Interval, i2::Interval)
+# Return:
+#   -1 when `i1` precedes `i2`,
+#   0 when `i1` overlaps with `i2`, and
+#   +1 when `i1` follows `i2`.
+function compare_overlap(i1::Interval, i2::Interval, isless::Function)
+    if isless(i1.seqname, i2.seqname)::Bool
+        return -1
+    elseif isless(i2.seqname, i1.seqname)::Bool
+        return +1
+    else  # i1.seqname == i2.seqname
+        if i1.last < i2.first
+            return -1
+        elseif i1.first > i2.last
+            return +1
+        else
+            return 0
+        end
+    end
+end
+
+# Faster comparison for `Base.isless`.  Note that `Base.isless` must be
+# consistent wtih `Base.cmp` to work correctly.
+function compare_overlap(i1::Interval, i2::Interval, ::typeof(Base.isless))
     c = cmp(i1.seqname, i2.seqname)
     if c != 0
         return c
