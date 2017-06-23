f127343ff10d91848ede63be1898c031196cc285
Ben J. Ward <ward9250@gmail.com>
2017-06-23 16:58:59 +0100

    Complete Bio.Seq decomposition (#468)
    * Complete Bio.Seq decomposition
    
    * Removed sequence tests, as BioSequences.jl has its own test suite.
    
    * minor test alteration

---
+++ src/Seq.jl
@@ -0,0 +1,18 @@
+# Bio.Seq
+# =======
+#
+# Module for biological sequences.
+#
+# This file is a part of BioJulia.
+# License is MIT: https://github.com/BioJulia/Bio.jl/blob/master/LICENSE.md
+
+
+
+
+module Seq
+
+using Reexport
+
+@reexport using BioSequences
+
+end  # module Bio.Seq
