12d92b5eee9e236d4dd0c5150102592203801ac4
Ben J. Ward <ward9250@gmail.com>
2017-08-07 16:15:55 +0100

    Make aliases of modules, instead of modules re-exporting modules.

--- src/Bio.jl
+++ src/Bio.jl
@@ -2,6 +2,16 @@ __precompile__()
 
 module Bio
 
+import BioSequences
+import GenomicFeatures
+import BioAlignments
+import Phylogenies
+
+const Seq = BioSequences
+const Intervals = GenomicFeatures
+const Align = BioAlignments
+const Phylo = Phylogenies
+
 include("declare.jl")
 include("Exceptions.jl")
 include("IO.jl")
@@ -11,15 +21,11 @@ include("ReaderHelper.jl")
 include("RecordHelper.jl")
 include("StringFields.jl")
 include("util/Util.jl")
-include("Seq.jl")
 include("services/Services.jl")
-include("Intervals.jl")
-include("Align.jl")
 include("util/tokenize.jl")
 include("util/indexing.jl")
 include("util/windows.jl")
 include("var/Var.jl")
-include("Phylo.jl")
 include("structure/Structure.jl")
 include("tools/Tools.jl")
 
