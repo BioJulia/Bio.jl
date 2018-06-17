3316faaa3e351a2b8cad1610595b96d5927f4e12
Ben J. Ward <ward9250@gmail.com>
2018-06-17 14:13:01 +0100

    Removed Phylogenies until it is updated

--- src/Bio.jl
+++ src/Bio.jl
@@ -5,18 +5,18 @@ module Bio
 import BioSequences
 import GenomicFeatures
 import BioAlignments
-import Phylogenies
+#import Phylogenies
 import BioStructures
 import GeneticVariation
 import BioServices
 
-const Seq = BioSequences
-const Intervals = GenomicFeatures
-const Align = BioAlignments
-const Phylo = Phylogenies
-const Structure = BioStructures
-const Var = GeneticVariation
-const Services = BioServices
-const Tools = BioTools
+#const Seq = BioSequences
+#const Intervals = GenomicFeatures
+#const Align = BioAlignments
+#const Phylo = Phylogenies
+#const Structure = BioStructures
+#const Var = GeneticVariation
+#const Services = BioServices
+#const Tools = BioTools
 
 end  # module Bio
