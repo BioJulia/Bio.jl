c2a3d955191efbf0a814be4dfd350fb8a9065865
Ben J. Ward <benjward@protonmail.com>
2020-05-27 00:27:38 +0100

    Update the documentation

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter, Bio, Pkg
+
+makedocs(
+    format = Documenter.HTML(),
+    sitename = "Bio.jl",
+    authors = replace(join(Pkg.TOML.parsefile("Project.toml")["authors"], ", "), r" <.*?>" => "" ),
+    pages = [
+        "Home" => "index.md"
+    ],
+    
+)
+
+deploydocs(
+    repo = "github.com/BioJulia/Bio.jl.git",
+    push_preview = true,
+    deps = nothing,
+    make = nothing
+)
