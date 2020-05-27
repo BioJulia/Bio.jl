30d7e5dd88f52cfe24aa5d2e2e90a98ef102aec6
Ben J. Ward <benjward@protonmail.com>
2020-05-27 01:13:15 +0100

    Undo manual

--- docs/make.jl
+++ docs/make.jl
@@ -5,16 +5,7 @@ makedocs(
     sitename = "Bio.jl",
     authors = replace(join(Pkg.TOML.parsefile("Project.toml")["authors"], ", "), r" <.*?>" => "" ),
     pages = [
-        "Home"    => "index.md",
-        "Install" => "man/install.md",
-        "Bio.Seq" => "man/seq.md",
-        "Bio.Align" => "man/alignments.md",
-        "Bio.Intervals" => "man/intervals.md",
-        "Bio.Var" => "man/var.md",
-        "Bio.Structure" => "man/structure.md",
-        "Bio.Util" => "man/util.md",
-        "Bio.Tools" => "man/tools.md",
-        "IO API" => "man/reading.md"
+        "Home"    => "index.md"
     ],
     
 )
